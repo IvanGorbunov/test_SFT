44cd51908ab5850e3a3f986fb0f867e8acfacb06
IvanGorbunov <falseprogrammerfirst@gmail.com>
2024-02-08 13:21:44 +0300

    fixed run-tests.yml

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -3,11 +3,18 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SQL_DEBUG = False
-RUN_IN_DOCKER = True
+RUN_IN_DOCKER = env.bool('RUN_IN_DOCKER', True)
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-        'default': env.db(var='DATABASE_URL_IN_DOCKER') if RUN_IN_DOCKER else env.db(),
+        'default': {
+            'ENGINE': env.str('SQL_ENGINE', 'django.db.backends.sqlite3'),
+            'NAME': env.str('SQL_DATABASE', os.path.join(BASE_DIR, "../db.sqlite3")),
+            'USER': env.str('SQL_USER', 'user'),
+            'PASSWORD': env.str('SQL_PASSWORD', 'password'),
+            'HOST': env.str('SQL_HOST', 'localhost'),
+            'PORT': env.str('SQL_PORT', '5432'),
+        }
 }
